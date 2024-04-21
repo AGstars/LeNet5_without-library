5496adce567e5a1cdae4dbbfa64f3ab570ac3749
TangerineSweet <42806456+AGstars@users.noreply.github.com>
2024-04-21 21:56:54 +0800

    Add files via upload

---
+++ CNN_numpy/losses.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def mean_squared_loss(y_predict, y_true):
+    """
+    均方误差损失函数
+    :param y_predict: 预测值,shape (N,d)，N为批量样本数
+    :param y_true: 真实值
+    :return:
+    """
+    loss = np.mean(np.sum(np.square(y_predict - y_true), axis=-1))  # 损失函数值
+    dy = y_predict - y_true  # 损失函数关于网络输出的梯度
+    return loss, dy
